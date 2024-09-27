011721fde4ee19a4f3710c8e0e32dee1eb2b0773
Rafi Cohn-Gruenwald <rafael.cohngruenwald@gmail.com>
2024-09-27 14:24:45 -0700

    add more documentation

--- code/build_orchestration_resources.sql
+++ code/build_orchestration_resources.sql
@@ -1,10 +1,11 @@
 {% macro build_orchestration_resources(prod_override=False) %}
     {#
-        By default, the tasks built into a development environment will have the CALL PROCEDURE
-        statement ommitted. This is out of an abundance of caution. Calling this macro with
-        prod_override set to True will build the tasks with the CALL PROCEDURE statement
-        included. Regardless of the prod_override parameter, any tasks built into a development
-        environment will build in a suspended state and must be enabled manually by the user.
+        By default, the tasks built into a development environment will have the
+        CALL PROCEDURE statement ommitted. This is out of an abundance of
+        caution. Calling this macro with prod_override set to True will build
+        the tasks with the CALL PROCEDURE statement included. Regardless of the
+        prod_override parameter, any tasks built into a development environment
+        will build in a suspended state and must be enabled manually by the user.
     #}
 
 {% if execute %}
@@ -17,6 +18,12 @@
         use schema {{target.schema}};
     {% endset %}
 
+    {#
+      Creates a loggin table. Because there is no easy way to view log or print
+      statements in a Snowflake python stored procedure, we return all the log
+      statements in an array at the conclusion of the procedure. Those logs are
+      then inserted into this table.
+    #}
     {% set create_log_table_sql -%}
         create or replace table {{target.database}}.{{target.schema}}.orchestration_logs(
             job_name string,
@@ -35,18 +42,24 @@
     {# Create SPROC #}
     {% do create_proc_dbtcloud_run_job() %}
 
-    {{ create_stream_generic(
+    {# creates a stream chacking DML changes on the specified table #}
+    {% do create_stream_generic(
       database = "<DATABASE>",
       schema = "<SCHEMA>",
       table = "<TABLE>"
-    )}}
+    )
+    %}
 
-    {{ create_task_generic(
+    {#
+      creates a task that will invoke the stored procedure to trigger the
+      dbt cloud job, if the specified stream(s) have data in them
+    #}
+    {% do create_task_generic(
       job_id = ######,
       job_name = "dbt Cloud Job Name",
       streams = ["dbt_orchestration_stream__<SCHEMA>__<TABLE>"],
       prod_override = prod_override
-    )}}
+    ) %}
 
 {% endif %}
 
--- code/external_access_integration.sql
+++ code/external_access_integration.sql
@@ -1,14 +1,27 @@
+/*
+  Create network rule.
+  This allows egress outside of snowflake.
+  This network rule is used by the external access integration to hit the dbt cloud api.
+*/
 create network rule <DATABASE>.<SCHEMA>.DBT_CLOUD_EGRESS
   type = host_port
   mode = egress
   value_list = ('cloud.getdbt.com')
 ;
 
+/*
+  Create secret.
+  This secret holds the access token used to authenticate with the dbt cloud api.
+*/
 create secret <DATABASE>.<SCHEMA>.DBT_CLOUD_ACCESS_TOKEN
     TYPE = GENERIC_STRING
     SECRET_STRING = '{"access_token":"..."}'
 ;
 
+/*
+  This integration allows a stored procedure to access the dbt cloud access token
+  and submit requests outside of Snowflake.
+*/
 create external access integration DBT_CLOUD_EXTERNAL_ACCESS_INTEGRATION
   allowed_network_rules = (DBT_CLOUD_EGRESS)
   allowed_authentication_secrets = ('<DATABASE>.<SCHEMA>.DBT_CLOUD_ACCESS_TOKEN')
